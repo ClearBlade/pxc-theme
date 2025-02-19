c22ff9ea772691fcc68629d8434b53d109fd5106
Ryan Ciminski <ryanciminski@Ryans-MacBook-Pro-1743.local>
2025-02-19 14:25:05 -0600

    added clearblade lib

--- code/services/pxc_uninstall/pxc_uninstall.js
+++ code/services/pxc_uninstall/pxc_uninstall.js
@@ -1,11 +1,60 @@
 /**
- * @typedef {{prefix: string, entity_id: string, component_id: string, mfe_settings: Record<string, unknown>}} InstallParams
- * @param {CbServer.BasicReq & {params: InstallParams}} req
+ * @typedef {{prefix: string, entity_id: string, component_id: string}} UninstallParams
+ * @param {CbServer.BasicReq & {params: UninstallParams}} req
  * @param {CbServer.Resp} resp
  */
 
 function pxc_uninstall(req, resp) {
-  const params = req.params;
-  //component uninstall behavior here, undo any steps done in the install service
-  resp.success('Success');
+  var params = req.params;
+  var entity_id = params.entity_id;
+  var component_id = params.component_id;
+  var systemKey = "dcd2b1fb0ceceeeeaad794a7a99a01"; 
+  var userToken = req.userToken;
+  var baseUrl = "https://demo.clearblade.com/api/v/1/collection/" + systemKey + "/custom_settings";
+  var brandQuery = "brand";
+  var brandUrl = baseUrl + brandQuery;
+
+  var deleteOptions = {
+      method: "DELETE",
+      headers: {
+          "ClearBlade-UserToken": userToken
+      }
+  };
+
+  fetch(brandUrl, deleteOptions)
+      .then(function (response) {
+          if (!response.ok) {
+              throw new Error("Failed to delete branding data: " + response.statusText);
+          }
+          return response.json();
+      })
+      .then(function (responseData) {
+          log("Branding data deleted successfully: " + JSON.stringify(responseData));
+          deleteThemeData();
+      })
+      .catch(function (error) {
+          log("Error deleting branding data: " + JSON.stringify(error));
+          deleteThemeData();
+      });
+
+  function deleteThemeData() {
+      var themeQuery = "entity_id" + encodeURIComponent(entity_id) + "component_id" + encodeURIComponent(component_id);
+      var themeUrl = baseUrl + themeQuery;
+
+      fetch(themeUrl, deleteOptions)
+          .then(function (response) {
+              if (!response.ok) {
+                  throw new Error("Failed to delete theme data: " + response.statusText);
+              }
+              return response.json();
+          })
+          .then(function (responseData) {
+              log("Theme data deleted successfully: " + JSON.stringify(responseData));
+              resp.success("PXC Theme Component Uninstalled Successfully!");
+          })
+          .catch(function (error) {
+              log("Error deleting theme data: " + JSON.stringify(error));
+              resp.error("Failed to uninstall PXC Theme Component: " + JSON.stringify(error));
+          });
+  }
 }
